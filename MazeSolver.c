93b4c3b7bd4fb8df52fe818c5af3c25f236ada37
pwmaxwell <pmaxwell2@luc.edu>
2019-09-05 02:58:19 -0500

    Takes the characters from the file, changes them into integers, and adds them into the array.

---
+++ MazeSolver.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int** maze;
+int rows = 2; //increased this number for testing purposes
+int columns = 0;
+
+
+void aloc_maze(){ // taken from class discussion
+	maze = malloc(rows * sizeof(int*));
+	for(int i = 0; i < rows; i++)
+		maze[i] = malloc(columns * sizeof(int*));
+}
+
+
+void getMaze(char* file_name){
+	char c;
+	char c2;
+	FILE* file = fopen(file_name, "r");	
+	
+	while ((c = getc(file)) != EOF){
+		//printf("%c", c); //remove this print statement and add a print_maze()
+		
+		if(c == '\n'){
+			columns++;
+			
+			break;
+		}
+		columns++;
+	}
+	while((c = getc(file)) != EOF){
+		if(c == '\n')
+			rows++;
+		
+	}
+	
+	aloc_maze();
+	fclose(file);
+	file = fopen(file_name, "r");	
+	for(int i = 0; i < rows; i++){
+		for (int j = 0; j < columns; j++){
+			c2= getc(file);
+			if(c2 == 'b')
+				maze[i][j] = 1;
+			//if(c2 == '\n')
+				//maze[i][j] = '\n';
+				
+			if(c2 == ' ')
+				maze[i][j] = 9;
+			if(c2 == 's')
+				maze[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+			printf("%i", maze[i][j]);
+		}
+		printf("\n");
+	}	
+	printf("\n");
+	
+	
+	//printf("%c", '\n');
+	//printf("%i", columns);
+	fclose(file);
+}
+
+int main(){
+	getMaze("maze1.txt");
+}
+
