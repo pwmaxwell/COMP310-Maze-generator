50c33e58f1ee0dd6ada561e6268dabae05cfb984
pwmaxwell <pmaxwell2@luc.edu>
2019-09-24 23:55:55 -0500

    cmake completed (i think)

---
+++ CMakeFiles/MazeSolver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MazeSolver.dir/MazeSolver.c.o"
+  "bin/MazeSolver.pdb"
+  "bin/MazeSolver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MazeSolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
